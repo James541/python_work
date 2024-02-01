684ecab7d9bacfc818ae942a982be59427cdfc71
James541 <96335020+James541@users.noreply.github.com>
2024-02-01 12:09:58 -0800

    added chapter 3 exercises

---
+++ Chapter 3/greetings.py
@@ -0,0 +1,4 @@
+names = ['Nelson', 'Mike', 'Trammell']
+print(f'Thats where they get you, {names[0]}.')
+print(f'Thats where they get you, {names[1]}.')
+print(f'Thats where they get you, {names[2]}.')
