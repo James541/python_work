7041240bc97787bd38c26fc2d790a42aee91a45b
James541 <96335020+James541@users.noreply.github.com>
2024-02-02 23:30:14 -0800

    chapeter 3 exercise

---
+++ Chapter 3/transportation.py
@@ -0,0 +1,4 @@
+transportation = ['Mercedes Benz', 'BMW 7 Series', '1970 Pontiac GTO' ]
+print(f'I would like to own a {transportation[0]}.')
+print(f'I would like to own a {transportation[1]}.')
+print(f'I would like to own a {transportation[2]}.')
