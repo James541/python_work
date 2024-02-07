fdbc001b563deab0307a38fde2843857b736683c
James541 <96335020+James541@users.noreply.github.com>
2024-02-06 23:32:40 -0800

    exercise 3-8 seeing the world in proper location

---
+++ Chapter 3/seeing_the_world_3_8.py
@@ -0,0 +1,18 @@
+locations = ['hawaii', 'tokyo', 'rome', 'london', 'paris']
+print(locations)
+
+print(sorted(locations))
+
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.sort()
+print(locations)
+
+locations.sort(reverse=True)
+print(locations)
