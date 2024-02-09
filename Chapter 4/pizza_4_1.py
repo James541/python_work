017ec3e27408ead65a450f84b53e6c461f5e9c73
James541 <96335020+James541@users.noreply.github.com>
2024-02-09 01:37:21 -0800

    chapter 4 pizza exercise

---
+++ Chapter 4/pizza_4_1.py
@@ -0,0 +1,4 @@
+pizzas = ['pepperoni', 'pepperoni and jalapeno','pepperoni and black olive' ]
+for pizza in pizzas:
+    print(f"You can't go wrong with a {pizza} pizza!")
+print('I really enjoy pepperoni on pizza.')
