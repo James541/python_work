5b57fefa7fd7184ebc05b98c227b9a3746aabaaf
James541 <96335020+James541@users.noreply.github.com>
2024-02-17 23:57:11 -0800

    worked on conditional tests exercise, added 4 tests

--- Chapter 5/5_1_conditional_tests.py
+++ Chapter 5/5_1_conditional_tests.py
@@ -1,6 +1,30 @@
 car = 'subaru'
+
 print("Is car == 'subaru'? I predict True.")
 print(car == 'subaru')
 
 print("\nIs car == 'audi'? I predict False.")
 print(car == 'audi')
+
+drinks = ['water', 'coffee', 'beer']
+
+print('\nDoes the list contain tea?')
+
+if 'tea' in drinks:
+    print('Yes.')
+else:
+    print('No it does not.')
+
+print('\nDoes the list contain coffee?')
+
+if 'coffee' in drinks:
+    print("Yes.")
+else:
+    print('No it does not.')
+
+print('\nAre there more than five items on the list?')
+
+if len(drinks) > 5:
+    print('Yes there are more than five items on the list.')
+else:
+    print('No.')
