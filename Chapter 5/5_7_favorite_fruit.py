8e27d2ae75c0e2d96f21c19b917a8cc983d7759e
James541 <96335020+James541@users.noreply.github.com>
2024-02-19 20:16:17 -0800

    5.7 fruit exercise

---
+++ Chapter 5/5_7_favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['apples', 'oranges', 'cherrys']
+
+if 'apples' in favorite_fruits:
+    print('Apples are a very fine fruit.')
+
+if 'oranges' in favorite_fruits:
+    print('Oranges are a fine and respectable fruit.')
+
+if 'cherrys' in favorite_fruits:
+    print('A good cherry is one of the finest fruits you can find.')
+
+if 'bananas' in favorite_fruits:
+    print('A banana makes a good smoothie, but I won\'t just eat one.')
+
+if 'kiwi' in favorite_fruits:
+    print('Decent but not one of my favorites.')
