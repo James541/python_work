05bde069b11155b7b4535bf7a67b25f95d5f0808
James541 <96335020+James541@users.noreply.github.com>
2024-02-20 20:15:16 -0800

    Chapter 6, Try it yourself exercises 1 and 2

---
+++ Chapter 6/6_1_persons.py
@@ -0,0 +1,12 @@
+person_info = {
+    'first_name': 'James',
+    'last_name': 'Wagner',
+    'age': 43,
+    'current_city': 'Medford', 
+}
+
+print(person_info['first_name'])
+print(person_info['last_name'])
+print(person_info['age'])
+print(person_info['current_city'])
+
---
+++ Chapter 6/6_2_favorite_number.py
@@ -0,0 +1,11 @@
+favorite_numbers = {
+    'james': 5,
+    'cillian': 2,
+    'amy': 42,
+    'finn': 10,
+    'joe': 9,
+}
+
+for key, favorite_number in favorite_numbers.items():
+    print(f'{key.title()}\'s favorite number is {favorite_number}!')
+
