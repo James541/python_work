5541c656b688cf6e34f162c17906c468fcdbf25f
James541 <96335020+James541@users.noreply.github.com>
2024-02-27 12:30:47 -0800

    Chapter 7 folder, four exercises

---
+++ Chapter 7/even_or_odd.py
@@ -0,0 +1,7 @@
+number = input('Please enter a number and I will tell you if it is even or odd: ')
+number = int(number)
+
+if number % 2 == 0:
+    print('Your number is even!')
+else:
+    print('Your number is odd!')
