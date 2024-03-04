a13e1a2639b39eba1dfe23b1c5586b3b3a282e95
James541 <96335020+James541@users.noreply.github.com>
2024-03-03 23:55:12 -0800

    Chapter 8 exercises

---
+++ Chapter 7/Chapter 8/8_1_message.py
@@ -0,0 +1,4 @@
+def display_message():
+    print('In Chapter 8, we are learning about writing functions in Python.')
+
+display_message()
---
+++ Chapter 7/Chapter 8/greeter2.py
@@ -0,0 +1,5 @@
+def greet_user(username):
+    """Display a simple greeeting."""
+    print(f'Hello, {username.title()}!')
+
+greet_user('james')
