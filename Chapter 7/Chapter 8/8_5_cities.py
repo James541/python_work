3a4cf8ab9e2c181c9781c5d2546c8c4dadc16fc5
James541 <96335020+James541@users.noreply.github.com>
2024-03-04 23:37:40 -0800

    Exercise 8-5 and formatted name exercise

---
+++ Chapter 7/Chapter 8/8_5_cities.py
@@ -0,0 +1,7 @@
+def describe_cities(name, country='the United States'):
+    print(f'{name} is in {country}.')
+
+describe_cities('Portland')
+describe_cities('Chicago')
+describe_cities('Paris', 'France')
+
