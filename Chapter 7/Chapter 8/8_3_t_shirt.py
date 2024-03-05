f972124025402260f81f9e0ae2b35475c0d1b4f1
James541 <96335020+James541@users.noreply.github.com>
2024-03-04 23:13:31 -0800

    Chapter 8, exercises 8-3 and 8-4

---
+++ Chapter 7/Chapter 8/8_3_t_shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, msg_text):
+    print(f'\nT-Shirt ordered!\nSize: {size}\nShirt-Text: {msg_text}')
+
+make_shirt('large', 'I cant believe I ate the whole thing.')
+make_shirt(msg_text='HELLO THERE!', size='XXL')
---
+++ Chapter 7/Chapter 8/8_4_large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size='Large', msg_text='I love Python'):
+    print(f'\nT-Shirt ordered!\nSize: {size}\nShirt-Text: {msg_text}')
+
+make_shirt()
+make_shirt(size='Medium')
+make_shirt(size='Small', msg_text='I cant believe I ate the whole thing.')
