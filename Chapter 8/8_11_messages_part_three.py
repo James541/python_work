061dbb672abca78c744e4d962eaf1bf477897f50
James541 <96335020+James541@users.noreply.github.com>
2024-03-10 21:25:57 -0700

    exercise 8.10 and 8.11

---
+++ Chapter 8/8_11_messages_part_three.py
@@ -0,0 +1,14 @@
+messages = ['wtf brother', 'hello good sir', 'omw', 'a number 6 with extra dip']
+printed_messages = []
+
+def show_messages(messages, printed_messages):
+    while messages:
+        current_message = messages.pop()
+        print(current_message)
+        printed_messages.append(current_message)
+
+
+show_messages(messages[:], printed_messages)
+
+print(messages)
+print(printed_messages)
---
+++ Chapter 8/8_9_messages.py
@@ -0,0 +1,7 @@
+messages = ['wtf brother', 'hello good sir', 'omw', 'a number 6 with extra dip']
+
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+show_messages(messages)
