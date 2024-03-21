ec6ab8cb45abf1897deafe1bec2c4e4519d81354
James541 <96335020+James541@users.noreply.github.com>
2024-03-20 23:20:27 -0700

    Chapter 9: Car and ElectricCar

--- Chapter 9/electric_car.py
+++ Chapter 9/electric_car.py
@@ -13,11 +13,12 @@ class Car:
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
     
+    
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print(f'This car has {self.odometer_reading} miles on it.')
-    
 
+    
     def update_odometer(self, mileage):
         """
         Set the odometer reading to the current value.
@@ -28,15 +29,18 @@ class Car:
         else:
             print('You can\'t roll back an odometer!')
 
+
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading"""
         self.odometer_reading += miles
 
-my_used_car = Car('subaru', 'outback', 2019)
-print(my_used_car.get_descriptive_name())
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to elecrtric vehicles."""
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__( make, model, year)
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+
+my_leaf=ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
