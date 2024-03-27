4392ab6ba73b54ae3e2b07fccfbae69ba0068e60
James541 <96335020+James541@users.noreply.github.com>
2024-03-27 00:37:47 -0700

    9-15 refactoring

--- Chapter 9/9_15_lottery_analysis.py
+++ Chapter 9/9_15_lottery_analysis.py
@@ -1,7 +1,7 @@
 from random import choice
 
 drawing_pool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E']
-my_ticket = '0C54'
+my_ticket = '542A'
 winner = 'no'
 attempts = 0
 
@@ -19,7 +19,7 @@ while winner != 'yes':
     winning_combo = draw_winner()
     attempts += 1
 
-    if str(winning_combo) == my_ticket or attempts > 1000000:
+    if winning_combo == my_ticket:
         print(winning_combo)
         winner = 'yes'
         print(f'You won! It took {attempts} attempts.')
