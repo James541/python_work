834a174488aba7234fa9cde0f5312edfd20eadc4
James541 <96335020+James541@users.noreply.github.com>
2024-03-28 02:07:15 -0700

    Birthday exercise

--- Chapter 10/pi_birthday.py
+++ Chapter 10/pi_birthday.py
@@ -8,5 +8,8 @@ pi_string = ''
 for line in lines:
     pi_string += line.lstrip()
 
-print(f'{pi_string[:52]}...')
-print(len(pi_string))
+birthday = input('Enter your birthday, in the form of mmddyy: ')
+if birthday in pi_string:
+    print('Yes!')
+else:
+    print('Nope!')
